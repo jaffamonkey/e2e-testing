8091afbef944bef456463730df2948c6126e8303
jaffamonkey <paullittlebury@gmail.com>
2018-01-29 00:01:25 +0000

    updates

--- coverage/server.js
+++
@@ -1,7 +0,0 @@
-const coverage = require('istanbul-middleware');
-const express = require('express');
-const config = require('./config');
-
-const app = express();
-app.use('/coverage', coverage.createHandler());
-app.listen(config.port);
