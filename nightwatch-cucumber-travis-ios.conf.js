6a29bf8ebb7d975e7e0a85a75c6862b2ddaceccf
Paul Littlebury <paullittlebury@gmail.com>
2018-05-02 21:40:55 +0200

    Update nightwatch-cucumber-travis-ios.conf.js

--- nightwatch-cucumber-travis-ios.conf.js
+++ nightwatch-cucumber-travis-ios.conf.js
@@ -24,7 +24,7 @@ module.exports = {
         start_process: false,
         server_path: seleniumServer.path,
         log_path: '',
-       // host: '127.0.0.1',
+        host: '127.0.0.1',
         port: 4444
     },
     appium: {
@@ -34,7 +34,7 @@ module.exports = {
         default: {
             launch_url: 'http://google.com',
             selenium_port: 4723,
-            // selenium_host: '127.0.0.1',
+            selenium_host: '127.0.0.1',
             silent: true,
             desiredCapabilities : {
               browserName : 'Safari',
