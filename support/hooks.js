cf1217d1511c810237913e357f30e6b0520304f4
Paul Littlebury <paullittlebury@gmail.com>
2018-05-03 19:44:24 +0200

    Update hooks.js

--- support/hooks.js
+++ support/hooks.js
@@ -4,9 +4,9 @@ var path = require('path');
 var {After, Before} = require('cucumber');
 
 // Before(function () {
-//     return client
-//     .url(client.launch_url)
-// });
+    return client
+    .url(client.launch_url)
+});
 
 After(function () {
     return client
