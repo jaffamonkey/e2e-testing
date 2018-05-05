327fd2e621afc253edead9aa1b5da92a1194f987
Paul Littlebury <paullittlebury@gmail.com>
2018-05-05 03:37:35 +0200

    Update hooks.js

--- support/hooks.js
+++ support/hooks.js
@@ -3,10 +3,10 @@ const reporter = require('cucumber-html-reporter');
 var path = require('path');
 var {After, Before} = require('cucumber');
 
-Before(function () {
-    return client
-    .url(client.launch_url)
- });
+// Before(function () {
+//     return client
+//     .url(client.launch_url)
+//  });
 
 After(function () {
     return client
