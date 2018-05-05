f8907411cc4a251f2cd9930a117275822c6c79eb
Paul Littlebury <paullittlebury@gmail.com>
2018-05-06 01:45:22 +0200

    Update nightwatch-cucumber-travis-ios.conf.js

--- nightwatch-cucumber-travis-ios.conf.js
+++ nightwatch-cucumber-travis-ios.conf.js
@@ -40,8 +40,8 @@ module.exports = {
               automationName: 'XCUITest',
               browserName : 'Safari',
               platformName : 'iOS',
-              platformVersion : '10.3',
-              deviceName : 'iPad Air 2',
+              platformVersion : '10.2',
+              deviceName : 'iPad Air',
               // noReset: false
               // "app": APP_PATH + "ios/PieDrive.app", // path for the ios app you want to test
             },
